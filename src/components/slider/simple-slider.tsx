118d20d12a586b8ce9fdc1992d15137eb2a2d4be
Ed <edwoulfe@gmail.com>
2022-12-29 14:13:41 -0500

    Implemented slider with all my projects and is updating description, links, title, etc with the current project that is being displayed on the slider. Need to add css.

--- src/components/slider/simple-slider.tsx
+++ src/components/slider/simple-slider.tsx
@@ -2,10 +2,10 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import "./simple-slider.scss";
-import { bdoProfitProject } from "./project-objects/bdoprofit";
-import { favoriteMoviesProject } from "./project-objects/favorite-movies";
-import { ffxivccProject } from "./project-objects/ffxivcc";
-import { lolChampionsProject } from "./project-objects/lol-champions";
+import { bdoProfitProject } from "../projects/project-objects/bdoprofit";
+import { favoriteMoviesProject } from "../projects/project-objects/favorite-movies";
+import { ffxivccProject } from "../projects/project-objects/ffxivcc";
+import { lolChampionsProject } from "../projects/project-objects/lol-champions";
 import youtube from "../../assets/icons/svg/youtube.svg";
 import desktop from "../../assets/icons/svg/desktop.svg";
 import github from "../../assets/icons/svg/github.svg";
@@ -14,40 +14,48 @@ import { Project } from "../../interfaces/project";
 import { useState } from "react";
 
 export default function SimpleSlider() {
-  const [currentProject, setCurrentProject] = useState<Project>(ffxivccProject);
+  const projectsArr = [ffxivccProject, lolChampionsProject, bdoProfitProject, favoriteMoviesProject];
+  const [currentProjectIndex, setCurrentProjectIndex] = useState<number>(0);
+
   let settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    initialSlide: currentProjectIndex,
+    beforeChange: setNewIndex,
   };
-  const projectsArr = [ffxivccProject, lolChampionsProject, bdoProfitProject, favoriteMoviesProject];
 
-  function createProjectsContainer(projects: Array<Project>) {
+  function setNewIndex(oldIndex: number, newIndex: number) {
+    setCurrentProjectIndex(newIndex);
+  }
+
+  function createSliderImages(projects: Array<Project>) {
     return projects.map((project, index) => {
       return (
-        <div>
+        <div key={"project-image-" + index}>
           <img src={project.image} alt="project" />
         </div>
       );
     });
   }
-  function createProjectContainer(project: Project) {
+
+  function createProjectContainer(currentProject: Project) {
     return (
       <div>
-        <h3>{project.name}</h3>
+        <h3>{currentProject.name}</h3>
         <ul className="project-links">
           <li
             onClick={() => {
-              window.open("https://www.youtube.com/watch?v=" + project.videoURL);
+              window.open("https://www.youtube.com/watch?v=" + currentProject.videoURL);
             }}>
             <img src={youtube} alt="youtube icon" />
             <span>Demo</span>
           </li>
           <li
             onClick={() => {
-              window.open(project.liveURL);
+              window.open(currentProject.liveURL);
             }}>
             <img src={desktop} alt="website icon" />
             <span>Website</span>
@@ -55,7 +63,7 @@ export default function SimpleSlider() {
 
           <li
             onClick={() => {
-              window.open(project.githubURL);
+              window.open(currentProject.githubURL);
             }}>
             <img src={github} alt="github icon" />
             <span>GitHub</span>
@@ -66,9 +74,10 @@ export default function SimpleSlider() {
   }
 
   return (
-    <div>
-      <div>{createProjectContainer(currentProject)}</div>
-      <Slider {...settings}>{createProjectsContainer(projectsArr)}</Slider>
+    <div className="project-info">
+      <div>{createProjectContainer(projectsArr[currentProjectIndex])}</div>
+      <Slider {...settings}>{createSliderImages(projectsArr)}</Slider>
+      <div>{projectsArr[currentProjectIndex].description}</div>
     </div>
   );
 }
