5100ea9aa3f2858c6620cb9b2b043fe130388f34
Ed <edwoulfe@gmail.com>
2022-12-28 19:36:15 -0500

    Put all state management and project management into the Simple Slider component so I can won't have to pass up any state meaninglessly when it can all be done within one component.

--- src/components/slider/simple-slider.tsx
+++ src/components/slider/simple-slider.tsx
@@ -2,14 +2,19 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import "./simple-slider.scss";
+import { bdoProfitProject } from "./project-objects/bdoprofit";
+import { favoriteMoviesProject } from "./project-objects/favorite-movies";
+import { ffxivccProject } from "./project-objects/ffxivcc";
+import { lolChampionsProject } from "./project-objects/lol-champions";
+import youtube from "../../assets/icons/svg/youtube.svg";
+import desktop from "../../assets/icons/svg/desktop.svg";
+import github from "../../assets/icons/svg/github.svg";
 
 import { Project } from "../../interfaces/project";
+import { useState } from "react";
 
-interface SimpleSliderProps {
-  projectsArr: Array<Project>;
-  setCurrentProject: React.Dispatch<React.SetStateAction<Project>>;
-}
-export default function SimpleSlider(data: { props: SimpleSliderProps }) {
+export default function SimpleSlider() {
+  const [currentProject, setCurrentProject] = useState<Project>(ffxivccProject);
   let settings = {
     dots: true,
     infinite: true,
@@ -17,6 +22,7 @@ export default function SimpleSlider(data: { props: SimpleSliderProps }) {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+  const projectsArr = [ffxivccProject, lolChampionsProject, bdoProfitProject, favoriteMoviesProject];
 
   function createProjectsContainer(projects: Array<Project>) {
     return projects.map((project, index) => {
@@ -27,6 +33,42 @@ export default function SimpleSlider(data: { props: SimpleSliderProps }) {
       );
     });
   }
+  function createProjectContainer(project: Project) {
+    return (
+      <div>
+        <h3>{project.name}</h3>
+        <ul className="project-links">
+          <li
+            onClick={() => {
+              window.open("https://www.youtube.com/watch?v=" + project.videoURL);
+            }}>
+            <img src={youtube} alt="youtube icon" />
+            <span>Demo</span>
+          </li>
+          <li
+            onClick={() => {
+              window.open(project.liveURL);
+            }}>
+            <img src={desktop} alt="website icon" />
+            <span>Website</span>
+          </li>
 
-  return <Slider {...settings}>{createProjectsContainer(data.props.projectsArr)}</Slider>;
+          <li
+            onClick={() => {
+              window.open(project.githubURL);
+            }}>
+            <img src={github} alt="github icon" />
+            <span>GitHub</span>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div>{createProjectContainer(currentProject)}</div>
+      <Slider {...settings}>{createProjectsContainer(projectsArr)}</Slider>
+    </div>
+  );
 }
